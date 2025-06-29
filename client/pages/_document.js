fbcf8e5cbb908783d8ce29c78df0fb0cbf0bd1e4
Jia Le <jle26531@gmail.com>
2025-06-29 18:56:34 +0800

    Fix og

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -4,13 +4,13 @@ export default function Document() {
   return (
       <Html lang="en">
           <Head/>
-          <meta property="twitter:image" content="og.png"/>
+          <meta property="twitter:image" content="https://ring.jiale0709.eu.org/og.png"/>
           <meta property="twitter:title" content="Jia Le's Ring"/>
           <meta property="og:site_name" content="Jia Le's Ring"/>
           <meta property="twitter:description" content="Jia Le's Ring"/>
           <meta property="description" name={'description'} content="Jia Le's Ring"/>
           <meta property="og:title" content="Jia Le's Ring"/>
-          <meta property="og:image" content="og.png"/>
+          <meta property="og:image" content="https://ring.jiale0709.eu.org/og.png"/>
           <meta property="og:site_name" content="Jia Le's Ring"/>
           <meta property="og:description" content="Jia Le's Ring"/>
           <meta property="og:url" content="https://ring.jiale0709.eu.org"/>
